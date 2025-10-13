208d65c394e01d96ac9f11d65baaf2d06af302f8
Clyde McQueen <clyde@mcqueen.net>
2025-10-12 17:54:22 -0700

    Add GetMapID, fixes #3

--- include/System.h
+++ include/System.h
@@ -174,6 +174,7 @@ public:
     // Information from most recent processed frame
     // You can call this right after TrackMonocular (or stereo or RGBD)
     int GetTrackingState();
+    long unsigned int GetMapID() const;
     std::vector<MapPoint*> GetTrackedMapPoints();
     std::vector<cv::KeyPoint> GetTrackedKeyPointsUn();
 
